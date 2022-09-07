f28b3b12604165c3ab26eb0d584a5ba7b085c106
Falender <tanguy.fargier@hotmail.fr>
2022-09-07 19:10:52 +0200

    NTM

---
+++ header.php
@@ -0,0 +1,11 @@
+/<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
